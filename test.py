4caf522d8fa7dbfff9fb16706e38a5686f5f8971
StorkST <vchd@pm.me>
2020-06-07 13:27:43 +0200

    structure of test.py

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
 import os
+from pathlib import Path
 import sys
+import unittest
+import glob
 
+PATH_TESTS = "./tests/*[!oracle]"
 
+def test_cluster_in_out(input, output):
+    def test():
+        a = Path(input)
+        b = Path(output)
+        assert a.exists()
+        assert b.exists()
+    return test
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    test_cases = glob.glob(PATH_TESTS)
+    test_cases.append("aa")
+
+    for case in test_cases:
+        input = case
+        output = input + "-oracle"
+        suite.addTest(unittest.FunctionTestCase(
+                test_cluster_in_out(input, output), description=input))
+
+    unittest.TextTestRunner().run(suite)
