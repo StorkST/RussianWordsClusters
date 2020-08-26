6bc6bf167f179d13a9a0125070b1e9430693d1d2
StorkST <vchd@pm.me>
2020-08-26 10:36:35 +0200

    add every verb prefixes from Wiki

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="RussianWordsClusters",
-    version="0.0.4",
+    version="0.0.5",
     author="Vincent Charrade",
     author_email="vchd@pm.me",
     description="Clustering russian words by multiple criterias",
